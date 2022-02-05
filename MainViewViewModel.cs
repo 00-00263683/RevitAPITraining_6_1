28552af0c3acdaf7f2f62ac365f1a22515269dc4
00-00263683 <matrosda@gmail.com>
2022-02-05 15:26:00 +0300

    Добавьте файлы проекта.

---
+++ MainViewViewModel.cs
@@ -0,0 +1,86 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using RevitAPITraningLibrary_6_1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_6_1
+{
+    public class MainViewViewModel
+    {
+
+        private ExternalCommandData _commandData;
+
+        public List<DuctType> DuctTypes { get; } = new List<DuctType>();
+        public List<Level> Levels { get; } = new List<Level>();
+        public DelegateCommand SaveCommand { get; }
+        public double LevelOffset { get; set; }
+        public List<XYZ> Points { get; } = new List<XYZ>();
+        public DuctType SelectedDuctType { get; set; }
+        public Level SelectedLevel { get; set; }
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            DuctTypes = DuctsUtils.GetDuctTypes(commandData);
+            Levels = LevelsUtils.GetLevels(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            LevelOffset = 0;
+            Points = SelectionsUtils.GetPoints(_commandData, "Выберите точки", ObjectSnapTypes.Endpoints);
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (Points.Count < 2 || SelectedDuctType == null || SelectedLevel == null)
+                return;
+            var curves = new List<Curve>();
+            for (int i = 0; i < Points.Count; i++)
+            {
+                if (i == 0)
+                    continue;
+                var prevPoint = Points[i - 1];
+                var currentPoint = Points[i];
+                Curve curve = Line.CreateBound(prevPoint, currentPoint);
+                curves.Add(curve);
+            }
+
+            MEPSystemType mepSystemType = new FilteredElementCollector(doc)
+                .OfClass(typeof(MEPSystemType))
+                .Cast<MEPSystemType>()
+                .FirstOrDefault(sysType => sysType.SystemClassification == MEPSystemClassification.SupplyAir);
+
+            using (var ts = new Transaction(doc, "Создание воздуховода"))
+            {
+                ts.Start();
+                foreach (var curve in curves)
+                {
+                    Duct currentDuct = Duct.Create(doc, mepSystemType.Id, SelectedDuctType.Id, SelectedLevel.Id, curve.GetEndPoint(0), curve.GetEndPoint(1));
+                    Parameter offsetParam = currentDuct.get_Parameter(BuiltInParameter.RBS_OFFSET_PARAM);
+                    offsetParam.Set(UnitUtils.ConvertToInternalUnits(LevelOffset, UnitTypeId.Millimeters));
+                }
+                ts.Commit();
+            }
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
